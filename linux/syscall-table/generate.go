53af3863d8c96c1e6317e89657ddd5585a565b4d
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-03-04 06:58:37 +0200

    feat(linux-sys-call-table): update to 7.0-rc2

--- linux/syscall-table/generate.go
+++ linux/syscall-table/generate.go
@@ -13,7 +13,7 @@ import (
 //go:generate go run generate.go
 
 func main() {
-	const Version = "6.19-rc2"
+	const Version = "7.0-rc2"
 
 	resp, err := http.Get("https://raw.githubusercontent.com/torvalds/linux/v" + Version + "/include/linux/syscalls.h")
 	if err != nil {
